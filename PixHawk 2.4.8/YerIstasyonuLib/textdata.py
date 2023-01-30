8912b0a04a560233737918f87001e66119f9bb0d
Yunus Efe Yılmaz <89478740+yunusefeyilmaz@users.noreply.github.com>
2023-01-30 13:45:14 +0300

    Update textdata.py

--- PixHawk 2.4.8/YerIstasyonuLib/textdata.py
+++ PixHawk 2.4.8/YerIstasyonuLib/textdata.py
@@ -1,48 +1,105 @@
 from pymavlink import mavutil
 
+def writedata(master):
+    lines = data(master)+basinc(master)+motor(master)+mod(master)
+    with open('data.txt', 'w') as f:
+        for line in lines:
+            f.write(f"{line} ")
+        f.write("\n")
+
 
 def data(master):
+    master.mav.command_long_send(master.target_system,master.target_component,
+                                     mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,
+                                     0,
+                                     30,0,0,0,0,0,0)
     attitude = master.recv_match(type='ATTITUDE', blocking=True)
     yaw = attitude.yaw
     roll = attitude.roll
     pitch = attitude.pitch
-    
+    data = [yaw,roll,pitch]
+    return data
     print("Yaw: ", yaw)
     print("Roll: ", roll)
     print("Pitch: ", pitch)
     
 def basinc(master):
+    master.mav.command_long_send(master.target_system,master.target_component,
+                                     mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,
+                                     0,
+                                     29,0,0,0,0,0,0)
     pressure = master.recv_match(type='SCALED_PRESSURE', blocking=True)
     pressure_abs = pressure.press_abs
     pressure_diff = pressure.press_diff
     temperature = pressure.temperature
-    
-    print("Absolute Pressure: ", pressure_abs)
-    print("Differential Pressure: ", pressure_diff)
-    print("Temperature: ", temperature)
-    
+    data = [pressure_abs]
+    return data
     
 def motor(master):
+    master.mav.command_long_send(master.target_system,master.target_component,
+                                     mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,
+                                     0,
+                                     36,0,0,0,0,0,0)
     engine = master.recv_match(type='SERVO_OUTPUT_RAW', blocking=True)
     # The SERVO_OUTPUT_RAW message contains data for 8 servo/engine outputs
     # You can access the data for each output using the following indices:
     # engine.servo1_raw, engine.servo2_raw, engine.servo3_raw, engine.servo4_raw
     # engine.servo5_raw, engine.servo6_raw, engine.servo7_raw, engine.servo8_raw
-    
-    print("Engine 1 Output: ", engine.servo1_raw)
-    print("Engine 2 Output: ", engine.servo2_raw)
-    print("Engine 3 Output: ", engine.servo3_raw)
-    print("Engine 4 Output: ", engine.servo4_raw)
-    print("Engine 5 Output: ", engine.servo5_raw)
-    print("Engine 6 Output: ", engine.servo6_raw)
-    print("Engine 7 Output: ", engine.servo7_raw)
-    print("Engine 8 Output: ", engine.servo8_raw)
-    #and so on
-    
+    data =[engine.servo1_raw,engine.servo2_raw,engine.servo3_raw,engine.servo4_raw,engine.servo5_raw,
+           engine.servo6_raw,engine.servo7_raw,engine.servo8_raw]
+    return data
+def decode_mode(base_mode, custom_mode):
+    """Decode flight mode from base_mode and custom_mode fields of HEARTBEAT message"""
+    mode = 'UNKNOWN'
+    if base_mode & mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED:
+        if custom_mode == 0:
+            mode = 'STABILIZE'
+        elif custom_mode == 1:
+            mode = 'ACRO'
+        elif custom_mode == 2:
+            mode = 'ALT_HOLD'
+        elif custom_mode == 3:
+            mode = 'AUTO'
+        elif custom_mode == 4:
+            mode = 'GUIDED'
+        elif custom_mode == 5:
+            mode = 'LOITER'
+        elif custom_mode == 6:
+            mode = 'RTL'
+        elif custom_mode == 7:
+            mode = 'CIRCLE'
+        elif custom_mode == 8:
+            mode = 'POSITION'
+        elif custom_mode == 9:
+            mode = 'LAND'
+        elif custom_mode == 10:
+            mode = 'OF_LOITER'
+        elif custom_mode == 11:
+            mode = 'DRIFT'
+        elif custom_mode == 13:
+            mode = 'SPORT'
+        elif custom_mode == 14:
+            mode = 'FLIP'
+        elif custom_mode == 15:
+            mode = 'AUTOTUNE'
+        elif custom_mode == 16:
+            mode = 'POSHOLD'
+        elif custom_mode == 17:
+            mode = 'BRAKE'
+        elif custom_mode == 18:
+            mode = 'THROW'
+        elif custom_mode == 19:
+            mode = 'AVOID_ADSB'
+        elif custom_mode == 20:
+            mode = 'GUIDED_NOGPS'
+    return mode
 def mod(master):
+    master.mav.command_long_send(master.target_system,master.target_component,
+                                     mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE,
+                                     0,
+                                     0,0,0,0,0,0,0)
     mode = master.recv_match(type='HEARTBEAT', blocking=True)
-    mode_name = master.mode_mapping()[mode.custom_mode]
+    mode_name = decode_mode(mode.base_mode, mode.custom_mode)
     armed = mode.base_mode & mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED
-    
-    print("Current Mode: ", mode_name)
-    print("Armed: ", armed)
+    data=[mode_name,armed]
+    return data

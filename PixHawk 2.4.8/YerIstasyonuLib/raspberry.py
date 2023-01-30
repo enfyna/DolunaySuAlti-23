910b1f4f2f1392c131fc42a544c99accf642de51
Yunus Efe Yılmaz <89478740+yunusefeyilmaz@users.noreply.github.com>
2023-01-30 13:47:17 +0300

    Update raspberry.py

--- PixHawk 2.4.8/YerIstasyonuLib/raspberry.py
+++ PixHawk 2.4.8/YerIstasyonuLib/raspberry.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 18 13:29:12 2023
-
-@author: Yunus
-"""
-
 import paramiko
 import cv2 
 import time
@@ -12,20 +5,15 @@ import time
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-client.connect('raspberrypi.local',username='dolunay',password="123456")
+client.connect('dolunay-desktop',username='dolunay',password="12345")
 
 sftp = client.open_sftp()
-
 while True:
-    time.sleep(0.01)
+    time.sleep(0.03)
     try:
-        sftp.get('kamera.png', 'photo.png')
-        img = cv2.imread('photo.png')
-        cv2.imshow("Image",img)
+        sftp.get('kamera'+'.png', 'photo.png')
     except:
         pass
     cv2.waitKey(1)
-    if ord('q') == cv2.waitKey(33):
-        break
+
 client.close()
-cv2.destroyAllWindows()

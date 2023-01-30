a8f4612e2b166685b0c16860ec7b87d056f8f204
Yunus Efe Yılmaz <89478740+yunusefeyilmaz@users.noreply.github.com>
2023-01-30 13:46:34 +0300

    Update disconnect.py

--- PixHawk 2.4.8/YerIstasyonuLib/disconnect.py
+++ PixHawk 2.4.8/YerIstasyonuLib/disconnect.py
@@ -1,7 +1,6 @@
-
 import subprocess
 
-raspberry_pi_ip = "raspberrypi"
+raspberry_pi_ip = "dolunay-desktop"
 password = "123456"
-command = f"ssh dolunay@{raspberry_pi_ip} 'sudo pkill python'"
+command = f"ssh dolunay@{raspberry_pi_ip} sudo pkill python"
 subprocess.run(command, shell=True)

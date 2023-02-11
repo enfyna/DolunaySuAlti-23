dc389b22b5257ba42666ff989d9dd385dec52f25
Ramazan Aslan <91965312+enfyna@users.noreply.github.com>
2023-02-11 16:57:35 +0300

    Update main.py

--- Gorev1/main.py
+++ Gorev1/main.py
@@ -1,16 +1,95 @@
-import sys , cv2 , time #, cProfile
+import sys, time , cProfile
 import Gorev1
 #import Gorev2
-#import Gorev3
+import Gorev3
 import DolunaySuAlti
 
-def yunusbaligi():
-    arac.hareket_et(400, 0, 0, 0)
-    pass
+def calistir(arac,baglanti_modu,secilengorev,gorev=None):
+    try:
+        if baglanti_modu == "USB":
+            if secilengorev == 1:
+                cap = cv2.VideoCapture(0)
+                cap.set(cv2.CAP_PROP_FRAME_WIDTH, 500)
+                cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 500)
+                gorev = Gorev1.Gorev(baglanti_modu,None)
+                gorev = gorev.modu
+            if secilengorev == 2:
+                pass
+            if secilengorev == 3:
+                gorev = Gorev3.Gorev(baglanti_modu,None)
+                pass
+            while True:
+                """
+                #Ornek kod : 
+                sonuc = gorev.calistir()
+                if sonuc[0] == None and sonuc[1] == None:
+                    yunusbaligi()
+                else:
+                    x = sonuc[0]
+                    y = sonuc[1]
+                    z = sonuc[2]
+                    r = sonuc[3]
+                    arac.hareket_et(x,y,z,r)
+                time.sleep(0.1)
+                """
+        elif baglanti_modu == "SITL":
+            if secilengorev == 1: #Gorevlere gereken degerleri ilet
+                gorev = Gorev1.Gorev(baglanti_modu,None)
+                gorev = gorev.modu
+                render_kare_sayisi = [28,350]
+            if secilengorev == 2:
+                render_kare_sayisi = [0,10]
+                pass
+            if secilengorev == 3:
+                gorev = Gorev3.Gorev(baglanti_modu,None)
+                render_kare_sayisi = [None,None]
+                pass
+            i = render_kare_sayisi[0] + 1
+
+            forward = int(input("Forward : "))
+            lateral = int(input("Lateral : "))
+            throttle = int(input("Throttle : "))
+            yaw = int(input("Yaw : "))
+            roll_deg = int(input("Roll_Deg : "))
+            pitch_deg = int(input("Pitch_Deg : "))
+            
+            while True:
+                i += 1
+                if i < render_kare_sayisi[0] or i > render_kare_sayisi[1]:
+                    i = render_kare_sayisi[0] + 1
+                
+                arac.hareket_et_v2(set_forward=forward, set_lateral=lateral, set_throttle=throttle, set_yaw=yaw, set_roll_deg=roll_deg, set_pitch_deg=pitch_deg)
+                time.sleep(1)
+        
+        elif baglanti_modu == "BAGLI_DEGIL":
+            if secilengorev == 1: #Gorevlere gereken degerleri ilet
+                gorev = Gorev1.Gorev(baglanti_modu,None)
+                gorev = gorev.modu
+                render_kare_sayisi = [28,350]
+            if secilengorev == 2:
+                render_kare_sayisi = [0,10]
+                pass
+            if secilengorev == 3:
+                gorev = Gorev3.Gorev(baglanti_modu,None)
+                render_kare_sayisi = [0,10]
+                pass
+            i = render_kare_sayisi[0] + 1
+            while True:
+                i += 1
+                if i < render_kare_sayisi[0] :
+                    i = render_kare_sayisi[0] + 1
+                elif i > render_kare_sayisi[1]:
+                    i = render_kare_sayisi[0] + 1
+            pass
+    except KeyboardInterrupt as e:
+        s = str(input("Gorev sonlandirilsin mi ? [e/h]"))
+        if s.lower() == "h":
+            calistir(arac, baglanti_modu, secilengorev, gorev)
+        
 
 # Profiler ayarla (Kod performasını incelemek için)
-#profiler = cProfile.Profile()
-#profiler.enable()
+profiler = cProfile.Profile()
+profiler.enable()
 
 # Calistirilacak olan gorevi sec
 try:    
@@ -20,78 +99,27 @@ except:
     secilengorev = 1
 print("Secili gorev : " + str(secilengorev))
 
-# Arac ile baglan
-#baglanti_modu = "USB"
+# Arac ile baglanma modlari 
+baglanti_modu = "USB" #Arac komutlarini ve gorev algoritmasini denemek icin
 baglanti_modu = "SITL"
+#baglanti_modu = "BAGLI_DEGIL" #Sadece gorev algoritmasini denemek icin
 
-arac = DolunaySuAlti.Dolunay(baglanti_modu)
-arac.set_mod("Manual")
-arac.set_arm(1)
+if baglanti_modu == "USB" or baglanti_modu == "SITL":
+    arac = DolunaySuAlti.Dolunay(baglanti_modu)
+    arac.set_mod("acro")
+    arac.set_arm(1)
 
-try:
-    if baglanti_modu == "USB":
-        if secilengorev == 1:
-            cap = cv2.VideoCapture(0)
-            cap.set(cv2.CAP_PROP_FRAME_WIDTH, 500)
-            cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 500)
-            cap_hx = int(cap.get(3)/2)
-            gorev = Gorev1.Gorev(baglanti_modu,cap)
-        if secilengorev == 2:
-            pass
-        if secilengorev == 3:
-            pass
-        while True:
-            sonuc = gorev.calistir("USB")
-            
-            if sonuc[0] == None and sonuc[1] == None:
-                yunusbaligi()
-            else:
-                x = cap_hx - sonuc[1]
-                y = sonuc[0]
-                z = sonuc[1]
-                r = sonuc[0]
-                arac.hareket_et(x,y,z,r)
-            time.sleep(0.01)
+calistir(arac,baglanti_modu,secilengorev)
 
-    elif baglanti_modu == "SITL":
-        if secilengorev == 1: #Gorevlere gereken degerleri ilet
-            gorev = Gorev1.Gorev(baglanti_modu,None)
-            render_kare_sayisi = [28,350]
-        if secilengorev == 2:
-            render_kare_sayisi = [None,None]
-            pass
-        if secilengorev == 3:
-            render_kare_sayisi = [None,None]
-            pass
-        i = render_kare_sayisi[0] + 1
-        while True:
-            i += 1
-            if i < render_kare_sayisi[0] or i > render_kare_sayisi[1]:
-                i = render_kare_sayisi[0] + 1
-            
-            sonuc = gorev.calistir(i)
-            
-            if sonuc[0] == None:
-                yunusbaligi()
-            else:
-                x = 250-sonuc[1]
-                y = sonuc[0]
-                z = sonuc[1]
-                r = sonuc[0]*2
-                arac.hareket_et(x,y,z,r)
-            time.sleep(0.1)
-except KeyboardInterrupt:
-    print("Gorev sonlandirildi.")
-
-# Araci disarm et
-arac.set_arm(0)
 # Kamerayi kapat
 if secilengorev < 3 and baglanti_modu == "USB":
     gorev.cap_yoket()
 # Arac baglantısını kapat
-if baglanti_modu == "USB":
+if baglanti_modu == "USB" or baglanti_modu == "SITL":
+    # Araci disarm et
+    arac.set_arm(0)
+    #Baglantiyi kapat
     arac.kapat()
 # Profiler bitir ve sonuclari yazdir
-#profiler.disable()
+profiler.disable()
 #profiler.print_stats("tottime")
-
